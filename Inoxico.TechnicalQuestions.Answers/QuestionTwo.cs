45832d9b05afcfd913e914a175fe8e2425b2397f
aethos <atharding511@gmail.com>
2022-04-26 20:06:56 +0200

    In-progress: Deepest PitDetermine when there is a pit

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -13,13 +13,30 @@ namespace Inoxico.TechnicalQuestions.Answers
             List<Tuple<int, int, int>> pits = new List<Tuple<int, int, int>>();
 
             bool counting = false;
+            int highestPoint = 0;
+
+
             for (int i = 0; i < points.Length; i++)
             {
-                if (points[i] > 0 && !counting)
+                if (points[i] < 0 && !counting)
                     continue;
-                if(!DetermineIncreasing(points[i],points[i+1]))
+
+                int point = points[i];
+                if (!DetermineIncreasing(point,points[i+1]))
                 {
-                    
+                    if (!counting)
+                    {
+                        highestPoint = point;
+                        counting = true;
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (counting && point >= 0)
+                    {
+
+                    }
                 }
 
             }
