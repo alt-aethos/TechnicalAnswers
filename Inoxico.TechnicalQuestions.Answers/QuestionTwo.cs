014c929e2b0f8b5c3471b74f5a1cc362399e81e2
aethos <atharding511@gmail.com>
2022-04-26 19:44:32 +0200

    In-Progress: Determine deepest pit Add functionality to determine when a pit is deepest and return array of all pits

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,7 +5,25 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private static Tuple<int,int,int>[] GetPits(int[] points)
+        {
+            List<Tuple<int, int, int>> pits = new List<Tuple<int, int, int>>();
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                
+            }
+        }
+
+        private static bool DetermineIncreasing(int a, int b)
+        {
+            if (a < b)
+                return true;
+            else
+                return false;
         }
     }
 }
