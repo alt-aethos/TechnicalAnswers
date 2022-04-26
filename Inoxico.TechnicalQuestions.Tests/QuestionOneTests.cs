e57ffd463eaf9a5063c79dd1d9dd041007bdf1ff
aethos <atharding511@gmail.com>
2022-04-26 19:34:30 +0200

    Done: Add another unit test for “Forget CVs..Save time . x x”

--- Inoxico.TechnicalQuestions.Tests/QuestionOneTests.cs
+++ Inoxico.TechnicalQuestions.Tests/QuestionOneTests.cs
@@ -6,12 +6,21 @@ namespace Inoxico.TechnicalQuestions.Tests
     public class QuestionOneTests
     {
         [Fact]
-        public void ExampleTest()
+        public void GivenUnitTest()
         {
             var input = $"We test coders. Give us a try?";
             var outcome = QuestionOne.GetLongestSentance(input);
 
             Assert.Equal(4, outcome);
+        }
+
+        [Fact]
+        public void AlternativeUnitTest()
+        {
+            var input = $"Forget CVs..Save time . x x";
+            var outcome = QuestionOne.GetLongestSentance(input);
+
+            Assert.Equal(2, outcome);
         }
     }
 }
