20021133148a8990d373730a5b67cd9907ee6880
aethos <atharding511@gmail.com>
2022-04-26 19:30:00 +0200

    In-Progress: Determin longest stentance in string

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,29 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            if (s.Length < 1)
+                throw new ArgumentException("string cannot be less than 1");
+            else if (s.Length > 100)
+                throw new ArgumentException("string cannot be greater than 100");
+
+            string[] stentances = s.Split('.');
+
+            int longestWordLength = 0;
+
+            foreach (string stentance in stentances)
+            {
+                int currentWordLength = 0;
+                string[] words = stentance.Split(' ');
+                foreach (string word in words)
+                {
+                    if (!string.IsNullOrEmpty(word))
+                        currentWordLength++;
+                }
+            }
+
+
+
+            return longestWordLength;
         }
     }
 }
