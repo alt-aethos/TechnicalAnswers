9528690175c9ea6b58df6913e00f1b1bd0fd6c15
aethos <atharding511@gmail.com>
2022-04-26 20:38:35 +0200

    In-progress: Determine deepest pit Replace tuple with class containing values Create Method in class to detemine depth Track lowest depth

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,12 +5,18 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            
+            Pit[] pits = GetPits(points);
+            int deepestPitLength = -1;
+
+            foreach(Pit pit in pits)
+            {
+                deepestPitLength = pit.GetDepth();
+            }
         }
 
-        private static Tuple<int,int,int>[] GetPits(int[] points)
+        private static Pit[] GetPits(int[] points)
         {
-            List<Tuple<int, int, int>> pits = new List<Tuple<int, int, int>>();
+            List<Pit> pits = new List<Pit>();
 
             //Variables to keep track of pit context
             bool counting = false;
@@ -18,10 +24,12 @@ namespace Inoxico.TechnicalQuestions.Answers
             int lowestPoint = 0;
 
 
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0; i < points.Length - 1; i++)
             {
                 if (points[i] < 0 && !counting)
                     continue;
+
+
 
                 int point = points[i];
                 if (!DetermineIncreasing(point,points[i+1]))
@@ -43,7 +51,7 @@ namespace Inoxico.TechnicalQuestions.Answers
                     if (counting && point >= 0)
                     {
                         //if we have been counting and the current point is above 0. stop counting and add values as current pit. Reset variables for future iterations
-                        pits.Add(new Tuple<int, int, int>(highestPoint, lowestPoint, point));
+                        pits.Add(new Pit(highestPoint, lowestPoint, point));
                         counting = false;
                         highestPoint = point;
                         lowestPoint = 0;
@@ -63,6 +71,27 @@ namespace Inoxico.TechnicalQuestions.Answers
                 return true;
             else
                 return false;
+        }
+
+        
+    }
+
+    public class Pit
+    {
+        public int P;
+        public int Q;
+        public int R;
+
+        public Pit(int p, int q, int r)
+        {
+            P = p;
+            Q = q;
+            R = r;
+        }
+
+        public int GetDepth()
+        {
+            return Math.Abs(P - Q);
         }
     }
 }
